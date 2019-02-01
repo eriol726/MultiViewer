4eab462d918e87587425e4766e515c2f4517dd84
eriol726 <eriol726@student.liu.com>
2019-02-01 10:35:16 +0100

    added color circles to expansion-panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { CdkDragDrop, moveItemInArray, 
-transferArrayItem, CdkDragEnter, CdkDragExit, 
-CdkDragStart, CdkDrag } from 
-'@angular/cdk/drag-drop';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -18,5 +15,24 @@ export class AppComponent {
     'Item 2',
     'Item 3',
   ]
+
+  done = [
+    'Get up',
+    'Brush teeth',
+    'Take a shower',
+    'Check e-mail',
+    'Walk dog'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
   //https://blog.angularindepth.com/exploring-drag-and-drop-with-the-angular-material-cdk-2e0237857290
 }
