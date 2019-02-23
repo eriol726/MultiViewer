cdf9f177121ddc324f84eb4964fad882b8f9263a
eriol726 <eriol726@student.liu.se>
2019-02-23 16:38:11 +0100

    multiple area graph with zoom and brush implemented

--- src/app/middle/middle.component.ts
+++ src/app/middle/middle.component.ts
@@ -2,10 +2,21 @@ import { Component, OnInit, ViewChild, ElementRef, Input, EventEmitter, Output, 
 import * as Plotly from 'plotly.js';
 import * as d3 from 'd3';
 import * as d3Array from 'd3-array';
+import * as d3Shape from 'd3-shape';
+import * as d3Axis from 'd3-axis';
+import * as d3Zoom from 'd3-zoom';
+import * as d3Brush from 'd3-brush';
+import * as d3TimeFormat from 'd3-time-format';
 import { csvToJson } from 'convert-csv-to-json/src/csvToJson.js';
 import { TEMPERATURES } from '../../data/temperatures';
 import { HttpClient } from '@angular/common/http';
 
+export interface Margin {
+  top: number;
+  right: number;
+  bottom: number;
+  left: number;
+}
 
 @Component({
   selector: 'app-middle',
@@ -21,19 +32,39 @@ export class MiddleComponent implements OnInit {
   private chart: any;
  // private width: number;
 //  private height: number;
-  private xScale: any;
-  private yScale: any;
-  private colors: any;
+
+  private margin: Margin;
+  private margin2: Margin;
+
+  private width: number;
+  private height: number;
+  private height2: number;
+
+  private svg: any;     // TODO replace all `any` by the right type
+
+  private x: any;
+  private x2: any;
+  private y: any;
+  private y2: any;
+
   private xAxis: any;
+  private xAxis2: any;
   private yAxis: any;
 
-  svg: any;
-    margin;
+  private context: any;
+  private brush: any;
+  private zoom: any;
+  private area: any;
+  private area2: any;
+  private focus: any;
+
+  private upperOuterArea: any;
+  private upperInnerArea: any;
+  private lowerOuterArea: any;
+
+
     g: any;
-    width: number;
-    height: number;
-    x;
-    y;
+
     z;
     line;
 
@@ -47,59 +78,112 @@ export class MiddleComponent implements OnInit {
       //console.log("jsonPromise: ", parseDate(rawData.lines.line1[0].date));
       //var tt = rawData[0].map((v) => {v.line.line1} );
 
-      console.log("TEMPERATURES: ", TEMPERATURES[0].values);
-      var line1 = TEMPERATURES.map((v) => v.values.map(function (d) {
-        return {
-            date:  d.date,
-            pct05: d.temperature / 1 -1,
-            // pct25: d.pct25 / 1000 +4,
-            pct50: d.temperature / 1 +0,
-            pct75: d.temperature / 1 +1,
-            pct95: d.temperature / 1 +2 
-          };
-        }
-      ));
-      console.log("line1: ", line1);
-      // var line1 = TEMPERATURES[0].values.map(function (d) {
-      //   return {
-      //     date:  d.date,
-      //     pct05: d.temperature / 1 -1,
-      //     // pct25: d.pct25 / 1000 +4,
-      //     pct50: d.temperature / 1 +0,
-      //     pct75: d.temperature / 1 +1,
-      //     pct95: d.temperature / 1 +2 
-      //   };
-      // });
-    
-      var line2 = rawData.lines.line2.map(function (d) {
-        return {
-          date:  parseDate(d.date),
-          pct05: d.pct50 / 2100,
-          // pct25: d.pct25 / 2000,
-          pct50: d.pct50 / 2000,
-          pct75: d.pct75 / 2000,
-          pct95: d.pct75 / 1900
-        };
-      });
-    
-      var markerPromise = d3.json('assets/markers.json');
-      markerPromise.then(function(markerData){
-        console.log("markerData: ", markerData);
-        var markers = markerData.map(function (marker) {
-          return {
-            date: parseDate(marker.date),
-            type: marker.type,
-            version: marker.version
-          };
-        });
-    
-        this.makeChart(line1,line2, markers);
-      }.bind(this));
     });
-    // if (this.data) {
-    //   this.updateChart();
-    // }
+    this.initMargins();
+    this.initSvg();
+
+    this.drawChart(TEMPERATURES);
+      
   }
+
+  private initMargins() {
+    this.margin = {top: 20, right: 20, bottom: 110, left: 40};
+    this.margin2 = {top: 430, right: 20, bottom: 30, left: 40};
+  }
+
+  private initSvg() {
+    this.svg = d3.select('svg');
+
+    this.width = +this.svg.attr('width') - this.margin.left - this.margin.right;
+    this.height = +this.svg.attr('height') - this.margin.top - this.margin.bottom;
+    this.height2 = +this.svg.attr('height') - this.margin2.top - this.margin2.bottom;
+
+    this.x = d3.scaleTime().range([0, this.width]);
+    this.x2 = d3.scaleTime().range([0, this.width]);
+    this.y = d3.scaleLinear().range([this.height, 0]);
+    this.y2 = d3.scaleLinear().range([this.height2, 0]);
+
+    this.xAxis = d3Axis.axisBottom(this.x);
+    this.xAxis2 = d3Axis.axisBottom(this.x2);
+    this.yAxis = d3Axis.axisLeft(this.y);
+
+    this.brush = d3Brush.brushX()
+        .extent([[0, 0], [this.width, this.height2]])
+        .on('brush end', this.brushed.bind(this));
+
+    this.zoom = d3Zoom.zoom()
+        .scaleExtent([1, Infinity])
+        .translateExtent([[0, 0], [this.width, this.height]])
+        .extent([[0, 0], [this.width, this.height]])
+        .on('zoom', this.zoomed.bind(this));
+
+
+    this.upperOuterArea = d3.area()
+    .curve(d3.curveBasis)
+    .x((d: any) => this.x(d.date) || 1)
+    .y0((d: any) => this.y(d.temperature )+20)
+    .y1((d: any) => this.y(d.temperature )+10);
+
+    this.upperInnerArea = d3.area()
+    .curve(d3.curveBasis)
+    .x((d: any) => this.x(d.date) || 1)
+    .y0((d: any) => this.y(d.temperature )+10)
+    .y1((d: any) => this.y(d.temperature )+0);
+
+    this.lowerOuterArea = d3.area()
+    .curve(d3.curveBasis)
+    .x((d: any) => this.x(d.date) || 1)
+    .y0((d: any) => this.y(d.temperature )+0)
+    .y1((d: any) => this.y(d.temperature )-10);
+
+
+    this.area2 = d3Shape.area()
+        .curve(d3Shape.curveMonotoneX)
+        .x((d: any) => this.x2(d.date))
+        .y0((d: any) => this.y2(d.temperature)+5)
+        .y1((d: any) => this.y2(d.temperature));
+
+    this.svg.append('defs').append('clipPath')
+        .attr('id', 'clip')
+        .append('rect')
+        .attr('width', this.width)
+        .attr('height', this.height);
+
+    this.focus = this.svg.append('g')
+        .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')')
+        .attr('class', 'focus');
+
+    this.context = this.svg.append('g')
+        .attr('class', 'context')
+        .attr('transform', 'translate(' + this.margin2.left + ',' + this.margin2.top + ')');
+  }
+
+  private brushed() {
+    if (d3.event.sourceEvent && d3.event.sourceEvent.type === 'zoom') return; // ignore brush-by-zoom
+    let s = d3.event.selection || this.x2.range();
+    //this.x.domain(s.map(this.x2.invert, this.x2));
+    // this.focus.selectAll('.areaOuterUpper').attr('d', function(d)  {return this.upperOuterArea(d.values)}.bind(this));
+    // this.focus.selectAll('.areaInner').attr('d', function(d)  {return this.upperInnerArea(d.values)}.bind(this));
+    // this.focus.selectAll('.areaOuterLower').attr('d', function(d)  {return this.lowerOuterArea(d.values)}.bind(this));
+    
+    //this.focus.select('.axis--x').call(this.xAxis);
+    this.svg.select('.zoom').call(this.zoom.transform, d3Zoom.zoomIdentity
+        .scale(this.width / (s[1] - s[0]))
+        .translate(-s[0], 0));
+  }
+
+  private zoomed() {
+      if (d3.event.sourceEvent && d3.event.sourceEvent.type === 'brush') return; // ignore zoom-by-brush
+      let t = d3.event.transform;
+      this.x.domain(t.rescaleX(this.x2).domain());
+      this.focus.selectAll('.areaOuterUpper').attr('d', function(d)  {return this.upperOuterArea(d.values)}.bind(this));
+      this.focus.selectAll('.areaInner').attr('d', function(d)  {return this.upperInnerArea(d.values)}.bind(this));
+      this.focus.selectAll('.areaOuterLower').attr('d', function(d)  {return this.lowerOuterArea(d.values)}.bind(this));
+      
+      this.focus.select('.axis--x').call(this.xAxis);
+      this.context.select('.brush').call(this.brush.move, this.x.range().map(t.invertX, t));
+  }
+
 
   ngOnChanges() {
     // if (this.chart) {
@@ -182,68 +266,7 @@ export class MiddleComponent implements OnInit {
       .attr('y', 85)
       .text('Median');
   }
-  
-  drawPaths (svg, data, x, y) {
-    var upperOuterArea = d3.area()
-      .x ((d: any) => x(d.date) || 1)
-      .y0((d: any) => y(d.pct95))
-      .y1((d: any) => y(d.pct75));
-
-    var upperInnerArea = d3.area()
-      .x ((d: any) => x(d.date) || 1)
-      .y0((d: any) => y(d.pct75))
-      .y1((d: any) => y(d.pct50));
-
-    var medianLine = d3.line()
-      .x((d: any) => x(d.date) || 1)
-      .y((d: any) => y(d.pct50));
-  
-    // var lowerInnerArea = d3.svg.area()
-    //   .interpolate('basis')
-    //   .x (function (d) { return x(d.date) || 1; })
-    //   .y0(function (d) { return y(d.pct50); })
-    //   .y1(function (d) { return y(d.pct25); });
-  
-    var lowerOuterArea = d3.area()
-      .x ((d: any) => x(d.date) || 1)
-      .y0((d: any) => y(d.pct50))
-      .y1((d: any) => y(d.pct05));
-
-    console.log("svg: ", svg);
-    this.g = svg.append('g');
-    let city = this.g.selectAll('.city')
-            .data(TEMPERATURES)
-            .enter().append('g')
-            .attr('class', 'city');
-    // svg.append('g')
-    svg.data(data);
-
-  
-    svg.append('path')
-      .attr('class', 'area upper outer')
-      .attr('d', upperOuterArea)
-      .attr('clip-path', 'url(#rect-clip)');
-  
-    svg.append('path')
-      .attr('class', 'area lower outer')
-      .attr('d', lowerOuterArea)
-      .attr('clip-path', 'url(#rect-clip)');
-  
-    svg.append('path')
-      .attr('class', 'area upper inner')
-      .attr('d', upperInnerArea)
-      .attr('clip-path', 'url(#rect-clip)');
-  
-    // svg.append('path')
-    //   .attr('class', 'area lower inner')
-    //   .attr('d', lowerInnerArea)
-    //   .attr('clip-path', 'url(#rect-clip)');
-  
-    // svg.append('path')
-    //   .attr('class', 'median-line')
-    //   .attr('d', medianLine)
-    //   .attr('clip-path', 'url(#rect-clip)');
-  }
+ 
   
   addMarker(marker, svg, chartHeight, x) {
     var radius = 32,
@@ -296,6 +319,82 @@ export class MiddleComponent implements OnInit {
       }, 1000 + 500*i);
     });
   }
+
+  private drawChart(data) {
+
+    this.x.domain(d3Array.extent(data[0].values, (d: any) => d.date));
+    this.y.domain([0, d3Array.max(data[0].values, (d: any) => d.temperature)]);
+    this.x2.domain(this.x.domain());
+    this.y2.domain(this.y.domain());
+
+
+    let cityGroup = this.focus.selectAll('.city')
+        .data(TEMPERATURES)
+        .enter().append("g")
+        .attr('class', 'city')
+
+
+    let cityArea = cityGroup.append('path')
+        .attr('class', 'areaOuterUpper')
+        .attr('d',function(d){
+            console.log("d first: ", d);
+            return this.upperOuterArea(d);
+        }.bind(this))
+        .attr('clip-path', 'url(#rect-clip)');
+
+    let cityArea3 = cityGroup.append('path')
+        .attr('class', 'areaInner')
+        .attr('d',function(d){
+            console.log("d second: ", d);
+            return this.upperInnerArea(d);
+        }.bind(this))
+        .attr('clip-path', 'url(#rect-clip)');
+
+    let cityArea2 = cityGroup.append('path')
+        .attr('class', 'areaOuterLower')
+        .attr('d',function(d){
+            console.log("d second: ", d);
+            return this.lowerOuterArea(d);
+        }.bind(this))
+        .attr('clip-path', 'url(#rect-clip)');
+
+
+    this.focus.append('g')
+    .attr('class', 'axis axis--x')
+    .attr('transform', 'translate(0,' + this.height + ')');
+    //.call(this.xAxis);
+
+    this.focus.append('g')
+    .attr('class', 'axis axis--y')
+    .call(this.yAxis);
+
+    this.context.append('path')
+        .datum(TEMPERATURES[0].values)
+        .attr('class', 'area')
+        .attr('d', this.area2);
+    
+    this.context.append('path')
+        .datum(TEMPERATURES[1].values)
+        .attr('class', 'area')
+        .attr('d', this.area2);
+
+    this.context.append('g')
+        .attr('class', 'axis axis--x')
+        .attr('transform', 'translate(0,' + this.height2 + ')')
+        .call(this.xAxis2);
+
+    this.context.append('g')
+        .attr('class', 'brush')
+        .call(this.brush)
+        .call(this.brush.move, this.x.range());
+
+    this.svg.append('rect')
+        .attr('class', 'zoom')
+        .attr('width', this.width)
+        .attr('height', this.height)
+        .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')')
+        .call(this.zoom);
+}
   
   makeChart (data,data1, markers) {
     var svgWidth  = 960,
@@ -331,7 +430,7 @@ export class MiddleComponent implements OnInit {
         .attr('height', chartHeight);
     //console.log("x: ", x(data));
     this.addAxesAndLegend(svg, xAxis, yAxis, margin, chartWidth, chartHeight);
-    this.drawPaths(svg, data, x, y);
+    //this.drawPaths(svg, data, x, y);
     //this.drawPaths(svg, data1, x, y);
     this.startTransitions(svg, chartWidth, chartHeight, rectClip, markers, x);
     
@@ -451,40 +550,40 @@ export class MiddleComponent implements OnInit {
 
   updateChart() {
     // update scales & axis
-    this.xScale.domain(this.data.map(d => d[0]));
-    this.yScale.domain([0, d3.max(this.data, d => d[1])]);
-    this.colors.domain([0, this.data.length]);
-    this.xAxis.transition().call(d3.axisBottom(this.xScale));
-    this.yAxis.transition().call(d3.axisLeft(this.yScale));
+    // this.xScale.domain(this.data.map(d => d[0]));
+    // this.yScale.domain([0, d3.max(this.data, d => d[1])]);
+    // this.colors.domain([0, this.data.length]);
+    // this.xAxis.transition().call(d3.axisBottom(this.xScale));
+    // this.yAxis.transition().call(d3.axisLeft(this.yScale));
 
-    let update = this.chart.selectAll('.bar')
-      .data(this.data);
+    // let update = this.chart.selectAll('.bar')
+    //   .data(this.data);
 
-    // remove exiting bars
-    update.exit().remove();
+    // // remove exiting bars
+    // update.exit().remove();
 
-    // update existing bars
-    this.chart.selectAll('.bar').transition()
-      .attr('x', d => this.xScale(d[0]))
-      .attr('y', d => this.yScale(d[1]))
-      .attr('width', d => this.xScale.bandwidth())
-      .attr('height', d => this.height - this.yScale(d[1]))
-      .style('fill', (d, i) => this.colors(i));
+    // // update existing bars
+    // this.chart.selectAll('.bar').transition()
+    //   .attr('x', d => this.xScale(d[0]))
+    //   .attr('y', d => this.yScale(d[1]))
+    //   .attr('width', d => this.xScale.bandwidth())
+    //   .attr('height', d => this.height - this.yScale(d[1]))
+    //   .style('fill', (d, i) => this.colors(i));
 
-    // add new bars
-    update
-      .enter()
-      .append('rect')
-      .attr('class', 'bar')
-      .attr('x', d => this.xScale(d[0]))
-      .attr('y', d => this.yScale(0))
-      .attr('width', this.xScale.bandwidth())
-      .attr('height', 0)
-      .style('fill', (d, i) => this.colors(i))
-      .transition()
-      .delay((d, i) => i * 10)
-      .attr('y', d => this.yScale(d[1]))
-      .attr('height', d => this.height - this.yScale(d[1]));
+    // // add new bars
+    // update
+    //   .enter()
+    //   .append('rect')
+    //   .attr('class', 'bar')
+    //   .attr('x', d => this.xScale(d[0]))
+    //   .attr('y', d => this.yScale(0))
+    //   .attr('width', this.xScale.bandwidth())
+    //   .attr('height', 0)
+    //   .style('fill', (d, i) => this.colors(i))
+    //   .transition()
+    //   .delay((d, i) => i * 10)
+    //   .attr('y', d => this.yScale(d[1]))
+    //   .attr('height', d => this.height - this.yScale(d[1]));
   }
 
 }
