c5546b9cabaaa06b5765f3533e253adba78d144c
eriol726 <eriol726@student.liu.com>
2019-08-27 08:48:45 +0200

    updating angular

--- src/app/websocket.service.ts
+++ src/app/websocket.service.ts
@@ -24,7 +24,7 @@ export class WebsocketService {
     console.log("this.socket: ", this.socket);
   }
 
-  private socket = io('http://192.168.2.194:3000');
+  private socket = io('http://localhost:3000');
   
   public lockedCM = [{"locked": false, "graphFactor": 0},
                       {"locked": false, "graphFactor": 0},
